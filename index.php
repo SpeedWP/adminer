ca0f070dd110b34dd28d8d4e87e7eacd6e1088da
speedwordpress <dani@speedword.press>
2017-11-22 20:49:34 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+
+ini_set('display_errors', 0);
+
+function adminer_object()
+{
+    // Required to run any plugin.
+    include_once "./plugins/plugin.php";
+
+    // Plugins auto-loader.
+    foreach (glob("plugins/*.php") as $filename) {
+        include_once "./$filename";
+    }
+
+    // Specify enabled plugins here.
+    $plugins = array(
+        new AdminerISPConfig('admin', 'admin', 'https://localhost:8080/remote/', false),
+    );
+
+    return new AdminerPlugin($plugins);
+}
+
+// Include original Adminer or Adminer Editor.
+include "./adminer.php";
